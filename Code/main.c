75ee814fbb5701fdf7a71d57d1e02b7d1fcbbcce
Frozen <sebamroz@onet.eu>
2016-12-04 17:55:16 +0100

    Update main.c

--- Code/main.c
+++ Code/main.c
@@ -1,59 +1,69 @@
 /*
  * main.c
  *
- *  Created on: 29.11.2016
- *      Author: frozen
+ *  Created on: 4 gru 2016
+ *      Author: Frozen
  */
-
 #include <avr/io.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
-//Sharp's
-#define SHARP1
-#define SHARP2
-#define SHARP3
-//Tcrt's
-#define TCRT1
-#define TCRT2
-#define TCRT3
-#define TCRT4
-//H_Bridges M_ == MOTOR Left/Right
-#define M_LA PD1
-#define M_LB PD2
-#define M_RA PD3
-#define M_RB PD4
+//Sharp's left/forward/right
+#define SH_L PB3
+#define SH_F PB4
+#define SH_R PD4
+
+//TCRT's
+#define TCRT1 PC0
+#define TCRT2 PC1
+#define TCRT3 PC2
+#define TCRT4 ADC7
+
+//H_Bridges M_ == MOTOR Left/Right A/B
+#define M_LA PB0
+#define M_LB PD7
+#define M_RA PD5
+#define M_RB PD6
+
+//PWM's left/right
+#define PWM_L PB1
+#define PWM_R PB2
+
+//Accelerometer
+#define ACMTR_INT1 PD3
+#define ACMTR_INT2 PD2
+#define ACMTR_SDA  PC4
+#define ACMTR_SCL  PC5
+
 
 typedef enum {left,forward,backward,right} directions;
 
 void init_io(void) {
-	DDRB = 0b00000111;
-	DDRC = 0b00000011;
-	DDRD = 0b00010111;
+	//DDRn   76543210
+	DDRB = 0b00000000;//to check
+	DDRC = 0b00000000;//to check
+	DDRD = 0b00000000;//to check
 
 	//PWM
-	TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << WGM10);
-	TCCR1B |= (1 << CS10) | (1 << WGM12);
+	TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << WGM10);//to check
+	TCCR1B |= (1 << CS10) | (1 << WGM12);//to check
 	//Interrupts
-	MCUCR |= (1 << ISC11);
-	GICR |= (1 << INT0) | (1 << INT1);
+	PCMSK0 |= (1 <<PCINT0) | (1 <<PCINT1) | (1 <<PCINT2);//wrong, has to be changed
+	PCICR |= (1 << SH_L) | (1 << SH_F) | (1 << SH_R);//to check
 
 }
 
-void soft_start(int mot_a, int mot_b){
+
+void motor_soft_start(int mot_a, int mot_b){
 
 }
 
 /*
- * To turn to the left we have to put left motor to
+ * Setting motors directions
  */
-void set_motors(directions dir){
+void set_motors_dir(directions dir){
 	switch(dir){
 	case left :
-		M_LA = 0;
-		M_LB = 1;
-		M_RA = 1;
-		M_RB = 0;
 		break;
 	case forward :
 		;
@@ -65,31 +75,29 @@ void set_motors(directions dir){
 		;
 		break;
 	default :
-		M_LA = 0;
-		M_LB = 0;
-		M_RA = 0;
-		M_RB = 0;
 		break;
 	}
 }
-
-ISR(INT0_vect){
+/*
+ * Setting motors velocity
+ */
+void set_motors_vel(int vel_L, int vel_R){
 
 }
-ISR(INT1_vect){
+
+ISR(PCINT0_vect){
 
 }
-ISR(INT2_vect){
+ISR(PCINT1_vect){
+
+}
+ISR(PCINT2_vect){
 
 }
 
 int main() {
 	init_io();
 	while (1) {
-		if (PIND & (SHARP1<<1)){
 
 		}
-
-	}
-
 }
